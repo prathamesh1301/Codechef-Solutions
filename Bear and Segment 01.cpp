b01a7860f960c522d1270353981b35e905bdde25
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-05-16 15:54:56 +0530

    SEGM01

---
+++ Bear and Segment 01.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+        string s;
+        cin>>s;
+        int ct=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1')
+                ct++;
+        }
+        int index;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1'){
+                index=i;
+                break;
+            }
+
+        }
+        int check=0;
+        for(int i=index;i<s.length();i++){
+            if(s[i]=='1')
+                check++;
+            else
+                break;
+        }
+        if(check==ct && ct>0)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+	}
+	return 0;
+}
