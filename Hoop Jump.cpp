d255f6e8e6d539b0790357486a0c5463005c7c63
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-06-01 18:15:44 +0530

    HOOPS

---
+++ Hoop Jump.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD= 1e9+7;
+const int N = 1e5+10;
+//int arr[N];
+
+int main(){
+
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int ans=(n/2)+1;
+		cout<<ans<<endl;
+}
+	
+	return 0;
+		
+}
