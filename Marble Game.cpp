1ddf66edc65fbf8ab75a4966dd618b0f117f099f
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-06-17 11:26:39 +0530

    ENC2020A

---
+++ Marble Game.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD= 1e9+7;
+const int N = 1e6+10;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long n;
+		cin>>n;
+		if(n==1)
+			cout<<'B'<<endl;
+		else if(n==3)
+			cout<<'A'<<endl;
+		else
+			cout<<'B'<<endl;
+
+
+
+	}	
+
+    
+	return 0;
+}
+	
+	
+		
