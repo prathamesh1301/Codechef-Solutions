4fe738116262e04db419d4cfdb0e8ce27b810822
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-07-13 10:43:01 +0530

    RELATIVE

---
+++ Relativity.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long m= 1e9+7;
+const int N = 1e5+10;
+#define ll long long
+
+void solve(){
+    ll c,g;
+    cin>>g>>c;
+    ll h=(c*c)/(2*g);
+    cout<<h<<endl;
+}
+
+int main(){   
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+
+    return 0;
+}
+
+
+
+
+	
+	
+
+	
+	
+		
