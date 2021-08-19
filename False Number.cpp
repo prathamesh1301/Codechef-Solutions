aeaf9d60bf4dcaabbba706489d8ea136ffe9b78d
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-08-19 22:45:18 +0530

    FALSNUM

---
+++ False Number.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll M=1e9+7;
+
+void  solve(){
+    string s;
+  cin>>s;
+
+  if(s[0]=='1')
+    s[0]='0';
+
+  cout<<1<<s<<"\n";
+
+
+}
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+
+
+return 0;
+}
